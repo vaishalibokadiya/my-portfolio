990f06b82f7bf57d58115e50fe7308f36f67e4cf
vaishalibokadiya <vaishali.bokadiya@gmail.com>
2023-06-13 23:27:07 +0530

    contact-layout

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { Button, Form, FormGroup } from "react-bootstrap";
 
 const Contact = () => {
-  return <div className="h-50rem">Contact</div>;
+  return (
+    <div className="block p-2rem">
+      <div className="flex-1 w-100">
+        <div className="w-60">
+          <h6>Let's get connected</h6>
+          <Form>
+            <Form.Group>
+              <Form.Control type="text" placeholder="Your name" />
+            </Form.Group>
+            <FormGroup>
+              <Form.Control type="email" placeholder="Your email" />
+            </FormGroup>
+            <FormGroup>
+              <Form.Control
+                as="textarea"
+                placeholder="Type your message here"
+              />
+            </FormGroup>
+            <Button type="submit" variant="primary" className="w-100">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
