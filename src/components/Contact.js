c44daa59a108d413681707d6b363971584342538
vaishalibokadiya <vaishali.bokadiya@gmail.com>
2023-06-21 22:11:34 +0530

    contact-styles-partial

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,34 +3,38 @@ import { Button, Form, FormGroup } from "react-bootstrap";
 
 const Contact = () => {
   return (
-    <div className="block p-2rem contact">
-      <div className="flex-1 w-100">
-        <div className="w-60">
-          <h2 className="other-lines">Let's get connected</h2>
-          <Form className="w-100">
+    <div className="flex-center h-82vh">
+      <div className="w-50">
+        <div>
+          <h2 className="text-blue">Let's get connected</h2>
+          <Form>
             <Form.Group>
               <Form.Control
-                className="w-100 form"
+                className="w-full form-input h-2rem"
                 type="text"
                 placeholder="Your name"
               />
             </Form.Group>
             <FormGroup>
               <Form.Control
-                className="w-100 form"
+                className="w-full form-input h-2rem"
                 type="email"
                 placeholder="Your email"
               />
             </FormGroup>
             <FormGroup>
               <Form.Control
-                className="w-100 form mb-50"
+                className="w-full form-input"
                 as="textarea"
                 placeholder="Type your message here"
               />
             </FormGroup>
-            <Button type="submit" variant="primary" className="w-100">
-              Submit
+            <Button
+              className="w-full btn letter-spacing"
+              type="submit"
+              variant="primary"
+            >
+              SUBMIT
             </Button>
           </Form>
         </div>
--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -4,15 +4,15 @@ import { TypeAnimation } from "react-type-animation";
 
 const Introduction = () => {
   return (
-    <div className="h-82vh flex-center">
-      <div className="text-center">
+    <div className="h-82vh flex-center m-ve">
+      <div className="text-center flex-1 h-100">
         <div className="intro">
           <h1 className="text-blue m-0">Hello! I am Vaishali,</h1>
           <h1 className="text-light-blue m-0">a Front-end Developer</h1>
           <h1 className="text-blue m-0">based in India.</h1>
         </div>
 
-        <p className="text-grey m-0 text-large">Welcome to my world</p>
+        <p className="text-grey m-2rem text-large">Welcome to my world</p>
       </div>
     </div>
   );
