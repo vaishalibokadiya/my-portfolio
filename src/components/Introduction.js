ba61969da8ce0a6fcf83dc744bb61bd7d7d1ee98
vaishalibokadiya <vaishali.bokadiya@gmail.com>
2023-06-23 19:08:44 +0530

    intro-motion

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,20 +1,50 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const Introduction = () => {
   return (
     <div className="h-95vh flex-center m-ve" id="intro">
       <div className="text-center flex-1 h-100">
         <div className="intro sm-intro sm-padding">
-          <h1 className="text-blue m-0 overflow-y-hidden">
+          <motion.h1
+            variants={fadeIn("up", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="text-blue m-0 overflow-y-hidden"
+          >
             Hello! I am Vaishali,
-          </h1>
-          <h1 className="text-light-blue m-0 overflow-y-hidden">
+          </motion.h1>
+          <motion.h1
+            variants={fadeIn("up", 0.4)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="text-light-blue m-0 overflow-y-hidden"
+          >
             a Front-end Developer
-          </h1>
-          <h1 className="text-blue m-0  overflow-y-hidden">based in India.</h1>
+          </motion.h1>
+          <motion.h1
+            variants={fadeIn("up", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="text-blue m-0  overflow-y-hidden"
+          >
+            based in India.
+          </motion.h1>
         </div>
 
-        <p className="text-grey m-2rem text-large">Welcome to my world</p>
+        <motion.p
+          variants={fadeIn("up", 0.6)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          className="text-grey m-2rem text-large"
+        >
+          Welcome to my world
+        </motion.p>
       </div>
     </div>
   );
